230baf428f5e0318abd2bb2e26fa59a63fffd229
Dongkyun Ham <jokerham@gmail.com>
2023-02-13 09:18:48 +0900

    Office : Fixed sign in

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import { Home } from './pages/portal/home/Home';
+import { Layout } from './pages/portal/layout/Layout';
 import { SignIn } from './pages/portal/layout/SignIn';
+import { NoPage } from './pages/portal/layout/NoPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <SignIn />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="signIn" element={<SignIn />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
